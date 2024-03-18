6788eb356025cb36deb3073697f58a79df1c982e
Jinliang <miku.cy@foxmail.com>
2024-03-18 16:12:29 +0800

    超声波相关IO引脚更改 PB10 -> PB3 PB11 -> PB4

--- APP/HCSR04/hcsr.h
+++ APP/HCSR04/hcsr.h
@@ -8,11 +8,11 @@
 //超声波硬件接口定义
 #define HCSR04_PORT GPIOB
 #define HCSR04_CLK  RCC_APB2Periph_GPIOB
-#define HCSR04_TRIG GPIO_Pin_10
-#define HCSR04_ECHO GPIO_Pin_11
+#define HCSR04_TRIG GPIO_Pin_3
+#define HCSR04_ECHO GPIO_Pin_4
 
-#define TRIG_Send   PBout(10)
-#define ECHO_Reci   PBin(11)
+#define TRIG_Send   PBout(3)
+#define ECHO_Reci   PBin(4)
 
 void Hcsr04_NVIC(void);
 void Hcsr04_Init(void);
