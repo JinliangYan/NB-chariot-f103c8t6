983d0bd5056ad7c27233d1d0ffe8fe6bfb7d2abe
Jinliang <miku.cy@foxmail.com>
2024-03-24 11:33:39 +0800

    build: _sbrk函数

--- CORE/syscalls.c
+++ CORE/syscalls.c
@@ -174,21 +174,3 @@ int _execve(char *name, char **argv, char **env)
   errno = ENOMEM;
   return -1;
 }
-
-caddr_t _sbrk(int incr)
-{
-  extern char end;		/* Defined by the linker */
-  static char *heap_end;
-  char *prev_heap_end;
-
-  if (heap_end == 0)
-  {
-    heap_end = &end;
-    /* give 16KB area for stacks and use the rest of memory for heap*/
-    heap_end += 0x4000;
-  }
-  prev_heap_end = heap_end;
-
-  heap_end += incr;
-  return (caddr_t) prev_heap_end;
-}
